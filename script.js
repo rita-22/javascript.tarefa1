bf50b3db7793f845b146512f4d71a6d7a84a63c7
rita-22 <ana2rita.m@gmail.com>
2024-11-18 12:21:36 +0000

    trabalho concluído

---
+++ script.js
@@ -0,0 +1,24 @@
+var name = "Rita";
+var age = "18"; 
+console.log(name)
+console.log (age);
+
+let ageMultiplied = age * 7;
+console.log("A idade multiplicada por 7 é: " + ageMultiplied);
+
+let amigos = [ "Dinis", "Rosita", "Leonor"];
+let características = [
+{
+    nome: "Dinis",
+    idade: 18,
+},
+{
+    nome: "Rosita",
+    idade: 18,
+},
+{
+    nome: "Leonor",
+    idade: 16,
+},
+]
+console.log(características);
